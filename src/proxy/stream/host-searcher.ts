a5c57f443dafd76b39ad4d05a6ac37bfaa6a258d
Ihor Tsykhovskyi <igorppok@gmail.com>
2022-01-31 19:45:22 +0200

    bugfix: subdomain wiwth dash symbol

--- src/proxy/stream/host-searcher.ts
+++ src/proxy/stream/host-searcher.ts
@@ -2,7 +2,7 @@
  * Searcher for Host HTTP Header
  */
 export const hostSearcher = (chunk: string) => {
-  const res = new RegExp("^Host: ([a-zA-Z0-9.]+)$", "m").exec(chunk);
+  const res = new RegExp("^Host: ([a-zA-Z0-9.-]+)$", "m").exec(chunk);
 
   return res !== null ? res[1] : null;
 };
